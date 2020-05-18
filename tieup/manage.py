74ca5a008e7b6b740c695a1f979491467733fe7d
Exeexe93 <e0318484@u.nus.edu>
2020-05-18 16:36:21 +0800

    Delete uneccesary comment in manage.py

--- tieup/manage.py
+++ tieup/manage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
-""" Main function for the application """
+"""Django's command-line utility for administrative tasks."""
 
 
 def main():
