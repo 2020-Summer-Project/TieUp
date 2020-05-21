9d9540b46c9245b19ae2b9eba702ffbb8917ebd3
Exeexe93 <e0318484@u.nus.edu>
2020-05-21 14:02:25 +0800

    Update model into postgresql database

--- tieup/users/models.py
+++ tieup/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    username = models.CharField(max_length=100, primary_key=True)
+    password = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=200)
+    email = models.EmailField()
